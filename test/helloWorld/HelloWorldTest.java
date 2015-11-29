a86bd9300e251e3f2f7d3bbddbfbac9d5e0a0878
StormGull <kllazo@live.com>
2015-11-29 12:18:07 -0500

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -6,10 +6,32 @@ import org.junit.Test;
 
 import uk.co.placona.helloWorld.HelloWorld;
 
-public class HelloWorldTest {
+public class HelloWorldTest
+{
+	private Hello hello;
+	
+	@After
+	public void teardown()
+	{
+		hello = null;
+	}
+	
 	@Test
-	public void testHellowWorld(){
-		HelloWorld hello = new HelloWorld();
-		assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
+	public void HelloNameTest()
+	{
+		hello = new Hello("World");
+		assertTrue("Hello name test failed", hello.toString().equals("Hello, World!"));
+	}
+	
+	@Test public void HelloBlankTest()
+	{
+		hello = new Hello("");
+		assertTrue("Hello blank test failed", hello.toString().equals("Hello!"));
+	}
+	
+	@Test public void HelloNullTest()
+	{
+		hello = new Hello(null);
+		assertTrue("Hello null test failed", hello.toString().equals("Hello!"));
 	}
 }
