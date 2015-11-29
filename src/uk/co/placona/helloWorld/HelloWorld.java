ad4bd7be3b9054b598ad0c0b836d4e7293277739
StormGull <kllazo@live.com>
2015-11-29 12:19:41 -0500

    Update HelloWorld.java

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -1,9 +1,37 @@
 package uk.co.placona.helloWorld;
 
-public class HelloWorld {
-
-	public String sayHello() {
-		return "Hello World";
+/**
+ * Hello may be printed to greet a selected
+ * individual.
+ */
+public class HelloWorld
+{
+	/**
+	 * Name of individual to be greeted
+	 */
+	private String name;
+	
+	/**
+	 * Default constructor - initializes this with a given name
+	 * 
+	 * @param name Name of individual to be greeted
+	 */
+	public HelloWorld(String name)
+	{
+		this.name = name;
 	}
-
+	
+	/**
+	 * Prints a greeting to an individual in the form, "Hello, [name]!"
+	 * If name was given as null or blank, prints as simple "Hello!"
+	 */
+	@Override
+	public String toString()
+	{
+		if (name == null || name.length() == 0)
+			return "Hello!";
+		else
+			return "Hello, " + name + "!";
+	}
 }
+
